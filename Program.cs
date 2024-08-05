ca90221f4e19b02e38cd5f50d772e278b85e1fe7
RandAlFaouri <rand.alfaouri.2003@gmail.com>
2024-08-05 13:35:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿ class Program
+{
+    static void Main(string[] args)
+    {
+        /*var num = 10;
+        string str = "10";
+        int num2 = str;*/
+        /* int numberInt = 100;
+         long numberLong = numberInt;
+         long nL = 1000;
+         if (nL <= Int32.MaxValue)
+         {
+
+
+             int nI = (int)nL;
+
+         }
+
+         double d = 1234.8;
+         int i = (int)d;
+         Console.WriteLine(i);*/
+        /*  int x = 10;
+          Object obj;
+          obj = x;
+          int y =(int) obj;*/
+        /*  string stringValue = "999999999999";
+          if (int.TryParse(stringValue, out int number))
+          {
+              Console.WriteLine(number);
+          }
+          else {
+              Console.WriteLine("Invalid number provided or doesnet fit integer";
+      }*/
+        /*  string stringValue = "999999999";
+          var i = Converter.ToInt32(stringValue);
+          Console.WriteLine(i);
+  */
+        /*  var number = 255;
+          var bytes = BitConverter.GetBytes(number);
+          foreach (var b in bytes)    
+              Console.WriteLine(b);*/
+        /* var number = 10;
+         var bytes = BitConverter.GetBytes(number);
+         foreach(var b in bytes)
+         {
+             var binary = Convert.ToString(b, 2).PadLeft(8,'0');
+             Console.WriteLine(binary);}*/
+        
+        var name = "rand";
+        char letters = name.ToCharArray();
+        foreach(var 1 in letters)
+        {
+            int ascii = Convert.ToInt32(1);
+            var output = $"'{1} "
+        }
+
+
+        
+    }
+}
